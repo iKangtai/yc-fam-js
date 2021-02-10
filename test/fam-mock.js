b99b72b068441feec3347759858b91246a16f7b3
luopk <luopk0911@gmail.com>
2021-02-10 16:01:51 +0800

    新增试纸单周期算法接口

---
+++ test/fam-mock.js
@@ -0,0 +1,35 @@
+const alInput = {
+  debug: 0,
+  param: { version: 1 },
+  userData: {
+    userAverageCycleLength: 28,
+    userCycleLengthError: 1,
+    userCycleRegularity: 1,
+    userAverageMenstruationLength: 5,
+    userAverageLuteumLength: 14
+  },
+  daysInput: [
+    {
+      impactTempFlag: 0,
+      BBT: 0,
+      ovulationResultByUser: 0,
+      ovulationResultByLH: 0,
+      cervicalMunusRecord: 0,
+      timestamp: 1461902400,
+      menstruationRecord: 10
+    },
+    {
+      impactTempFlag: 0,
+      BBT: 0,
+      ovulationResultByUser: 0,
+      ovulationResultByLH: 0,
+      cervicalMunusRecord: 0,
+      timestamp: 1461988800,
+      menstruationRecord: 10
+    }
+  ]
+}
+
+export default {
+  alInput
+}
--- test/fam-sdk.spec.js
+++ test/fam-sdk.spec.js
@@ -3,43 +3,13 @@ import { v4 as uuidv4 } from 'uuid'
 import _ from 'lodash'
 import assert from 'assert'
 import output from './output-check-data'
+import alInput from './fam-mock'
 
 const fam = new YCFam(
   '100200',
   '6e1b1049a9486d49ba015af00d5a0',
   'luopk@ikangtai.com'
 )
-const alInput = {
-  debug: 0,
-  param: { version: 1 },
-  userData: {
-    userAverageCycleLength: 28,
-    userCycleLengthError: 1,
-    userCycleRegularity: 1,
-    userAverageMenstruationLength: 5,
-    userAverageLuteumLength: 14
-  },
-  daysInput: [
-    {
-      impactTempFlag: 0,
-      BBT: 0,
-      ovulationResultByUser: 0,
-      ovulationResultByLH: 0,
-      cervicalMunusRecord: 0,
-      timestamp: 1461902400,
-      menstruationRecord: 10
-    },
-    {
-      impactTempFlag: 0,
-      BBT: 0,
-      ovulationResultByUser: 0,
-      ovulationResultByLH: 0,
-      cervicalMunusRecord: 0,
-      timestamp: 1461988800,
-      menstruationRecord: 10
-    }
-  ]
-}
 
 describe('Test FAM Algorithms', () => {
   it('Basic Algorithm', () => {
@@ -63,6 +33,38 @@ describe('Test FAM Algorithms', () => {
   it('Test Algorithm Cache', () => {
     return fam
       .getFAMDays(uuidv4(), alInput)
+      .then((res) => {
+        const alData = res
+        if (_.isEqual(alData, output.output1)) {
+          assert.ok(true)
+        } else {
+          assert.fail(
+            JSON.stringify(alData) +
+              '\n==============\n' +
+              JSON.stringify(output.output1)
+          )
+        }
+      })
+      .catch((err) => {
+        assert.fail(err)
+      })
+  }).timeout(15000)
+
+  it('Test LH Cycle Analysis', () => {
+    let lhInput = [
+      {
+        timestamp:1611901359,
+        value:5,
+        brand:7,
+      },
+      {
+        timestamp:1611903359,
+        value:15,
+        brand:7,
+      }
+    ]
+    return fam
+      .getLHCycleAnalysis(uuidv4(), lhInput)
       .then((res) => {
         const alData = res
         if (_.isEqual(alData, output.output1)) {
